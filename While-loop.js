e5b43544ce64a21040f212c34740b95813649532
Ezabulbary <ezabul.bary@gmail.com>
2022-02-05 22:42:59 +0600

    while loop odd and even number

--- While-loop.js
+++ While-loop.js
@@ -1,7 +1,23 @@
 var eggGiven = 0;
 
-while(eggGiven > 10){
-    console.log(Egg Den Plz..!!);
+while(eggGiven < 10){
+    console.log("Egg Den Plz..!!");
     eggGiven++;
     console.log(eggGiven);
+}
+
+// odd number
+
+var number = 1;
+while(number <= 30){
+    console.log(number);
+    number = number + 2;
+}
+
+// even number 
+
+var number = 0;
+while (number <= 30) {
+    console.log(number);
+    number = number + 2;
 }
